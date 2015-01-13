3bd2a5f619c584daf4bc5714749c543e849bf985
Mike Forsyth <mikeforsyth11@gmail.com>
2015-01-13 16:55:08 -0700

    Done writting initial code, now for debugging.

--- HotPlate.cpp
+++ HotPlate.cpp
@@ -1,44 +1,87 @@
-#include <vector>;
+#include <vector>
+#include <iostream>
+#include <chrono>
+#include <fstream>
 
 using std::vector;
 
-double when();
-
 int main()
 {
+	std::ofstream file_out("hotPlateFinished.txt", std::ofstream::out);
 	const float PLATE_SIZE = 1024;
 	const float NEUTRAL_TEMP = 50;
 	//vectors one initialized to having all 50's
-	vector<vector<float>> previous_plate (1024, vector<float>(PLATE_SIZE, NEUTRAL_TEMP));
+	//vector<vector<float>> previous_plate (PLATE_SIZE, vector<float>(PLATE_SIZE, NEUTRAL_TEMP));
+	vector<vector<float>> previous_plate;
 	vector<vector<float>> current_plate;
 
 
 	//initialize vector to have correct starting temperatures
-
-	//bottom row
+	//Make them all have 50
+	for (int i = 0; i < PLATE_SIZE; ++i)
+	{
+		vector<float> tmp;
+		previous_plate.push_back(tmp);
+		for (int j = 0; j < PLATE_SIZE; ++j)
+		{
+			previous_plate[i].push_back(50);
+		}
+	}
+		//bottom row
 	for (int i = 0; i < PLATE_SIZE; ++i)
 	{
 		previous_plate[PLATE_SIZE-1][i] = 100;
 	}
-	//mid section
+		//mid section
 	for (int i = 0; i < 331; ++i)
 	{
 		previous_plate[400][i] = 100;
 	}
-	//single point
+		//single point
 	previous_plate[200][500] = 100;
+	current_plate = previous_plate;
 
 
 	//Start iterating
+		//Start time
+	auto start_time = std::chrono::high_resolution_clock::now();
 
-	//Start time
+		//Run
+	int converged = 0;
+	int cycles = 0;
+	do
+	{
+		converged = 0;
+		//Determine new plate
+		for (int i = 1; i < PLATE_SIZE; ++i)
+		{
+			for (int j = 1; j < PLATE_SIZE; ++j)
+			{
+				//new temperature
+				current_plate[i][j] = (previous_plate[i + 1][j] + previous_plate[i - 1][j] +
+					previous_plate[i][j + 1] + previous_plate[i][j - 1] + 4 * previous_plate[i][j]) / 8;
+				//steady state check
+				if (abs(previous_plate[i][j] - (previous_plate[i + 1][j] + previous_plate[i-1][j] + 
+												previous_plate[i][j+1] + previous_plate[i][j-1]) / 4) > 0.1)
+				{
+					++converged;
+				}
+			}
+		}
+
+		//Swap
+		previous_plate = current_plate;
+
+	} while (converged > 0);
 
 
-}
 
-double when()
-{
-	struct timeval tp;
-	gettimeofday(&tp, NULL);
-	return((double)tp.tv_sec + (double)tp.tv_usec*1e-6);
+
+
+	//End time
+	auto end_time = std::chrono::high_resolution_clock::now();
+
+	auto time_span = std::chrono::duration_cast<std::chrono::duration<double>>(end_time - start_time);
+	file_out << "Total running time: " << time_span.count() << " seconds\n";
+	file_out << "Iterations: " << cycles;
 }
